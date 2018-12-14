c59f663fa0c230a42845b572fde526734943d910
Parisa Yalsavar <p.yalsavar@outlook.com>
2018-12-14 08:58:05 +0000

    Merged PR 46: A10
    Programming Assignment 10 : Hash Tables and Hash Functions
    
    Related work items: #37

---
+++ A10/A10/RabinKarp.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TestCommon;
+
+namespace A10
+{
+    public class RabinKarp : Processor
+    {
+        public RabinKarp(string testDataName) : base(testDataName) { }
+
+        public override string Process(string inStr) =>
+            TestTools.Process(inStr, (Func<string, string, long[]>)Solve);
+
+
+
+        public long[] Solve(string pattern, string text)
+        {
+            Random r = new Random();
+            long x = r.Next(1, (int)BigPrimeNumber - 1);
+            List<long> occurrences = new List<long>();
+
+            long pHash = HashingWithChain.PolyHash(pattern, 0, pattern.Length, x: x);
+
+            long[] tHashes = PreComputeHashes(text, pattern.Length, BigPrimeNumber, x);
+
+            for(int i=0; i<= text.Length - pattern.Length; i++)
+            {
+                if (pHash == tHashes[i])
+                        if (pattern.Equals(text.Substring(i, pattern.Length)))
+                            occurrences.Add(i);
+            }
+            
+            return occurrences.ToArray();
+        }
+        
+
+        public const long BigPrimeNumber = 1000000007;
+
+        public static long[] PreComputeHashes(
+            string T, 
+            int P,
+            long p,
+            long x
+            )
+        {
+            int textLength = T.Length;
+
+            long[] hashes = new long[textLength - P + 1];
+            hashes[T.Length - P] = HashingWithChain.PolyHash(
+                                T, start : textLength - P, count : P , p: p , x: x);
+            long y = 1;
+            for (int i = 1; i <= P; i++)
+                y = (y * x) % p;
+
+            for (int i = textLength - P - 1; i >= 0; i--)
+                hashes[i] = ( ( (x * hashes[i + 1])  + T[i] - (y * T[i + P]) ) % p + p )% p;
+
+            return hashes;
+        }
+    }
+}
---
+++ A10/A10Tests/GradedTests.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using A10;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestCommon;
+
+namespace A10.Tests
+{
+    [TestClass()]
+    public class GradedTests
+    {
+        [TestMethod(), Timeout(5000)]
+        [DeploymentItem("TestData", "A10_TestData")]
+        public void SolveTest()
+        {
+            Processor[] problems = new Processor[] {
+                new PhoneBook("TD1"),
+                new HashingWithChain("TD2"),
+                new RabinKarp("TD3")
+            };
+
+            foreach (var p in problems)
+            {
+                TestTools.RunLocalTest("A10", p.Process, p.TestDataName);
+            }
+        }
+
+
+        [TestMethod()]
+        [DeploymentItem("TestData", "A10_TestData")]
+        public void PhoneBookTest()
+        {
+            PhoneBook p = new PhoneBook("TD1");
+            TestTools.RunLocalTest("A10", p.Process, p.TestDataName);
+        }
+
+        [TestMethod()]
+        [DeploymentItem("TestData", "A10_TestData")]
+        public void HashingWithChainTest()
+        {
+            HashingWithChain p = new HashingWithChain("TD2");
+            TestTools.RunLocalTest("A10", p.Process, p.TestDataName);
+        }
+
+        [TestMethod()]
+        [DeploymentItem("TestData", "A10_TestData")]
+        public void RabinKarpTest()
+        {
+            RabinKarp p = new RabinKarp("TD3");
+            TestTools.RunLocalTest("A10", p.Process, p.TestDataName);
+        }
+
+        /// <summary>
+        /// This test is just to help you test your
+        /// PreComputeHashes function. It is not graded
+        /// </summary>
+        [TestMethod()]
+        public void PreComputeHashesTest()
+        {
+            string testStr = "aaaa";
+            int patternLen = 2;
+            long[] H = RabinKarp.PreComputeHashes(
+                testStr, patternLen, 101, 3);
+
+            long[] expectedHash = new long[testStr.Length - patternLen + 1];
+
+            for (int i = testStr.Length - patternLen; i >= 0; i--)
+                 expectedHash[i] =
+                    HashingWithChain.PolyHash(testStr, i, patternLen, p: 101, x : 3);
+             
+
+            CollectionAssert.AreEqual(expectedHash, H);
+        }
+    }
+}
